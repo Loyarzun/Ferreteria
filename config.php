ff0bc1971acfe246e29bdb9be5bd61eedb2a1c52
Loyarzun <43767323+Loyarzun@users.noreply.github.com>
2018-11-26 13:18:08 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'us-cdbr-iron-east-01.cleardb.net';
-$dbname = 'heroku_5af2830eb0bb922';
+$dbname = 'heroku_5af2830eb0bb992';
 $user = 'bf5cbab22d87fc';
 $password = 'c6373471';
 $charset = 'utf8';
